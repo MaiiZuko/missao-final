b1f1bcf7081e54cb602d3c61b6269861a1037d5d
Gustavo Drews <ti10@unimednoroesters.com.br>
2024-11-27 14:38:52 -0300

    comentarios

--- server.js
+++ server.js
@@ -1,51 +1,40 @@
-// Carregar variáveis de ambiente
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const pool = require('./config/db'); // Configuração do banco de dados
-const routes = require('./routes/routes'); // Arquivo de rotas
-const errorMiddleware = require('./middlewares/errorMiddleware'); // Middleware de erros
+const pool = require('./config/db');
+const routes = require('./routes/routes');
+const errorMiddleware = require('./middlewares/errorMiddleware');
 
-// Carregar variáveis de ambiente
 dotenv.config();
 
-// Validação das variáveis do .env
 const requiredEnvVariables = ['PORT', 'DB_HOST', 'DB_PORT', 'DB_USER', 'DB_PASSWORD', 'DB_NAME'];
 requiredEnvVariables.forEach((key) => {
   if (!process.env[key]) {
     console.error(`Erro: Variável de ambiente ${key} não está configurada no arquivo .env.`);
-    process.exit(1); // Encerra a execução se faltar uma variável
+    process.exit(1);
   }
 });
 
-// Inicializando o Express
 const app = express();
 
-// Middleware para interpretar JSON nas requisições
 app.use(express.json());
 
-// Middleware para permitir CORS
 app.use(cors());
 
-// Configuração de rotas
 app.use('/api', routes);
 
-// Middleware para tratamento de erros
 app.use(errorMiddleware);
 
-// Definindo a porta para o servidor
 const port = process.env.PORT || 3001;
 
-// Iniciando o servidor
 const server = app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
 
-// Encerramento gracioso (Graceful Shutdown) - Fechar conexões ao encerrar
 process.on('SIGINT', async () => {
   console.log('\nEncerrando o servidor...');
   try {
-    await pool.end(); // Fecha o pool de conexões com o banco
+    await pool.end();
     console.log('Conexão com o banco de dados encerrada.');
   } catch (err) {
     console.error('Erro ao encerrar o pool de conexões:', err);
@@ -53,6 +42,6 @@ process.on('SIGINT', async () => {
 
   server.close(() => {
     console.log('Servidor finalizado.');
-    process.exit(0); // Finaliza o processo de forma limpa
+    process.exit(0);
   });
 });
